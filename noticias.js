b3c3e7f24f2e8c4f6a366d2e9891207fb6d90546
carloseb90 <carlosembus6@gmail.com>
2026-08-24 15:36:18 -0300

    version 1.3

--- noticias.js
+++ noticias.js
@@ -10,23 +10,28 @@ async function cargarNoticiasLocales() {
       throw new Error(`Error en la petición: ${respuesta.status}`);
     }
     const datos = await respuesta.json();
-    console.log(datos);
     // Accedemos a la propiedad "articles" de tu JSON
-    const noticiasInvertidas = datos.articles.reverse();
-    desplegarNoticias(noticiasInvertidas);
+    //const noticiasInvertidas = datos.articles.reverse();
+    const ultimasCuatro = datos.articles.slice(-3).reverse();
+    console.log(ultimasCuatro);
+
+    desplegarNoticias(ultimasCuatro);
   } catch (error) {
     console.error("Error al cargar el archivo de noticias:", error);
   }
 }
 
 function desplegarNoticias(listaArticulos) {
+  console.log(listaArticulos);
   const contenedor = document.getElementById("contenedor-noticia");
+  // 2. Separar la primera (más reciente) de las otras tres
+  const [noticiaPrincipal, ...otrasNoticias] = listaArticulos;
 
   // Limpiamos el contenedor
   contenedor.innerHTML = "";
 
   // 2. Iterar sobre cada objeto noticia del archivo JSON
-  listaArticulos.forEach((noticia) => {
+  /*listaArticulos.forEach((noticia) => {
     // 3. Crear el diseño de la tarjeta usando los datos exactos del JSON
     const tarjeta = `
       <a href="${noticia.url}" class="tarjeta">
@@ -37,8 +42,33 @@ function desplegarNoticias(listaArticulos) {
     `;
     // 4. Inyectar la tarjeta al contenedor
     contenedor.innerHTML += tarjeta;
+  });*/
+
+  let html = `
+      <a href="${noticiaPrincipal.url}" class="tarjeta noticia-principal">
+        <img src="${noticiaPrincipal.urlToImage}" alt="${noticiaPrincipal.title}">
+        <small>${noticiaPrincipal.date}</small>
+        <h3>${noticiaPrincipal.title}</h3>
+      </a>  
+      `;
+
+  // 6. Recorrer las otras 3 noticias e integrarlas (Clase pequeña)
+  otrasNoticias.forEach((noticia) => {
+    html += `
+        <a href="${noticia.url}" class="tarjeta noticia-secundaria">
+          <img src="${noticia.urlToImage}" alt="${noticia.title}">
+          <small>${noticia.date}</small>
+          <h3>${noticia.title}</h3>
+        </a>
+        `;
   });
+
+  //html += `</div>`; // Cerrar el contenedor del bloque secundario
+  // 7. Inyectar todo el HTML construido en la página
+  contenedor.innerHTML = html;
 }
 
+// Ejecutar la función automáticamente cuando cargue la página
+//document.addEventListener("DOMContentLoaded", cargarNoticiasLocales);
 // Iniciar el proceso
 cargarNoticiasLocales();
